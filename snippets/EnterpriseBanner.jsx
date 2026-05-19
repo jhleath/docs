24636b449f2d74ee5aefb49e520fe3d19b6d14ca
wj-e2b <weilu.jia@e2b.dev>
2026-05-18 18:26:12 -0700

    Make the enterprise banner generic and fix otel capitalization (#218)

---
+++ snippets/EnterpriseBanner.jsx
@@ -0,0 +1,10 @@
+export const EnterpriseBanner = () => {
+  return (
+    <Note>
+      This feature is available for Enterprise customers only. If you're
+      interested in this feature, please book a call with our team{" "}
+      <a href="https://e2b.dev/contact">here</a> or contact us at{" "}
+      <a href="mailto:enterprise@e2b.dev">enterprise@e2b.dev</a>.
+    </Note>
+  );
+};
